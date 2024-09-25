c2defb99ec1e3d651b4f66fa4d64edace87e915d
Marcus Weber <marcus.wallau@gmail.com>
2024-09-25 13:09:50 +0000

    feat: install nvim in codespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Hello Codespace"
+cd /tmp
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+tar xvzf nvim-linux64.tar.gz -C ~/.local/share/
+cd ~/.local/bin/
+ln -sf ~/.local/share/nvim-linux64/bin/nvim nvim
+rm -rf /tmp/nvim-linux64.tar.gz
+echo "nvim installed"
